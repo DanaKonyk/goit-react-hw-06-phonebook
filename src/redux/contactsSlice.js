c1e3aed799c5c908d43808c509f1e7bd5f174b52
DanaKonyk <132483408+DanaKonyk@users.noreply.github.com>
2023-12-11 11:42:57 +0200

    state again

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,18 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-
-const contactsInitialState = { contacts: [] };
-
 const contactsSlice = createSlice({
   name: 'contacts',
- initialState: contactsInitialState,
+ initialState: [],
   reducers: {
     addContact(state, action) {
       console.log('Current state:', state);
-      return { ...state, contacts: [...state.contacts, action.payload] };
+      state.contacts = [...state.contacts, action.payload];
     },
     deleteContact(state, action) {
-      return { ...state, contacts: state.contacts.filter(contact => contact.id !== action.payload) };
+      state.contacts = state.contacts.filter(el => el.id !== action.payload);
     },
   },
 });
